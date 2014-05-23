6c460120f26b219ab015d89df4bb7fb58de48ee8
Stefano <stefanouesc@gmail.com>
2014-05-22 22:04:36 -0300

    Criado "EsqueciSenha"

---
+++ application/models/DbTable/AlteracaoSenha.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_AlteracaoSenha extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'alteracao_senha';
+    protected $_private = 'AS_ID';
+    
+}
+
